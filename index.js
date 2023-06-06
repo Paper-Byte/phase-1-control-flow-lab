859f606693548c07ef6016a1f37d9f1db913d31c
Noah <noahhughesph@gmail.com>
2023-06-06 17:31:38 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(tripDistance) {
+  if (tripDistance <= 400) return "This one is on me!";
+  else if (tripDistance > 400 && tripDistance < 2000)
+    return "That will be twenty bucks.";
+  else if (tripDistance < 2500) return "I will gladly take your thirty bucks.";
+  else return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(tripCity) {
+  if (tripCity === "NYC") return "Ok, sounds good.";
+  return "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipStatus) {
+  if (tipStatus === "generous") return "Thank you so much.";
+  else if (tipStatus === "not as generous") return "Thank you.";
+  return "Bye.";
 }
